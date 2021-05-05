4fc7e55dc6a943db83f3e81153ae656c3a44bd85
KyrylOTheRylo <kiril.mosiychuk@gmail.com>
2021-05-06 00:47:05 +0300

    ver 3

--- new.py
+++ new.py
@@ -11,11 +11,12 @@ G = nx.read_adjlist("2.txt")
 
 nx.draw_circular(G, with_labels=True, font_weight='bold', node_color="black", edge_color="black",font_color="w")
 plt.savefig('simple.png')
-
+plt.show()
 pos = {x: [float(x)//2, math.sin(float(x)*(math.pi/1.5))] for x in G.nodes()}
 pos["8"]=[3.4,0]
 pos["9"]=[4.4,-0.1]
 pos["10"]=[5.1,0.3]
 pos["13"]=[5,2]
 nx.draw(G, pos=pos, with_labels=True, font_weight='bold', node_color="black", edge_color="black",font_color="w")
+plt.show()
 plt.savefig('processed.png')
