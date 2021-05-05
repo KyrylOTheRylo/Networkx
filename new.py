58dc23f2b2b8bf5c76390c71cc39ffd409c87b00
KyrylOTheRylo <78597936+KyrylOTheRylo@users.noreply.github.com>
2021-05-06 00:10:08 +0300

    Create new.py

---
+++ new.py
@@ -0,0 +1,20 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import math
+my_graph = nx.Graph()
+f = open("2.txt","r")
+plt.figure(figsize=(8,8))
+
+
+plt.axes().set_aspect("equal",adjustable="datalim")
+G = nx.read_adjlist("2.txt")
+
+nx.draw_circular(G, with_labels=True, font_weight='bold', node_color="black", edge_color="black",font_color="w")
+plt.savefig('simple.png')
+
+pos = {x: [float(x)//2, math.sin(float(x)*(math.pi/1.5))] for x in G.nodes()}
+pos["8"]=[3.4,0]
+pos["9"]=[4.4,-0.1]
+pos["10"]=[5.1,0.3]
+pos["13"]=[5,2]
+nx.draw(G,pos=pos, with_labels=True, font_weight='bold', node_color="black", edge_color="black",font_color="w")
