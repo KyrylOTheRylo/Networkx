6f57084568f1f5d7cf7cf223e0f59c228ec45635
KyrylOTheRylo <kiril.mosiychuk@gmail.com>
2021-05-07 01:58:21 +0300

    version 5.0

--- new.py
+++ new.py
@@ -1,22 +1,90 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import math
+import random
+
+
+def set_own_pos():
+    pos = {x: [float(x) // 2, math.sin(float(x) * (math.pi / 1.5))] for x in G.nodes()}
+    pos["8"] = [3.4, 0]
+    pos["9"] = [4.4, -0.1]
+    pos["10"] = [5.1, 0.3]
+    pos["13"] = [5, 2]
+    return pos
+
+
+def new_edges(tuple_):
+    G.remove_edges_from(tuple_)
+
+    nx.draw(G, pos=set_own_pos(), with_labels=True, font_weight='bold', node_color="black", edge_color="black",
+            font_color="w")
+    G.add_edges_from(tuple_)
+    nx.draw_networkx_edges(G, pos=set_own_pos(), edgelist=tuple_, edge_color='r')
+
+
+"""
+PART 1
+"""
 my_graph = nx.Graph()
 f = open("2.txt", "r")
 plt.figure(figsize=(8, 8))
 
-
-plt.axes().set_aspect("equal",adjustable="datalim")
+plt.axes().set_aspect("equal", adjustable="datalim")
 G = nx.read_adjlist("2.txt")
 
 nx.draw_circular(G, with_labels=True, font_weight='bold', node_color="black", edge_color="black", font_color="w")
 plt.savefig('simple.png')
 plt.show()
-pos = {x: [float(x)//2, math.sin(float(x)*(math.pi/1.5))] for x in G.nodes()}
-pos["8"]=[3.4,0]
-pos["9"]=[4.4,-0.1]
-pos["10"]=[5.1,0.3]
-pos["13"]=[5,2]
-nx.draw(G, pos=pos, with_labels=True, font_weight='bold', node_color="black", edge_color="black", font_color="w")
+plt.close()
+
+"""
+PART 2
+"""
+
+nx.draw(G, pos=set_own_pos(), with_labels=True, font_weight='bold', node_color="black", edge_color="black",
+        font_color="w")
 plt.savefig('processed.png')
 plt.show()
+plt.close()
+"""
+PART 3
+"""
+
+
+def part3():
+    n = 1
+    tmp_edges_global = []
+    for x in (nx.connected_components(G)):
+        tmp = nx.subgraph(G, x)
+        print(f"-------Begin of the component {n}-------")
+
+        x = sorted(x, key=float)
+
+        print(f'nodes = {len(x)} ')
+        print(f'edges = {len(G.edges(x))}')
+        eccentr = nx.eccentricity(tmp)
+        for node_ in x:
+            print(f"~~~~~~~~Node {node_}~~~~~~~~")
+            print(f'''degree: {G.degree[node_]}''')
+            print(f'''eccentricity: {eccentr[node_]}''')
+        print("")
+        diam = nx.diameter(tmp, eccentr)
+        print(f'''Radius of the component {n} = {nx.radius(tmp, eccentr)}''')
+        print(f'''Diameter of the component {n} = {diam}''')
+        tmp_edges = []
+
+        for node1 in x:
+            for node2 in x:
+                for path in (nx.all_simple_edge_paths(tmp, node1, node2)):
+                    if len(list(path)) == 1 + diam:
+                        tmp_edges.append(path)
+        random_edges = random.choice(tmp_edges)
+        tmp_edges_global.extend(random_edges)
+
+        print(f"--------End of the component {n}--------\n")
+        n += 1
+    new_edges(tmp_edges_global)
+    plt.show()
+
+
+part3()
