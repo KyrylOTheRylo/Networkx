421e908714d98e2af1cd82dc170f95a8142770b5
KyrylOTheRylo <kiril.mosiychuk@gmail.com>
2021-05-11 14:34:25 +0300

    FINAL

--- new.py
+++ new.py
@@ -5,6 +5,10 @@ import random
 
 
 def set_own_pos():
+    """
+    own nodes pattern
+    :return: pos (for networkx) of nodes in the Graph after optimization
+    """
     pos = {x: [float(x) // 2, math.sin(float(x) * (math.pi / 1.5))] for x in G.nodes()}
     pos["8"] = [3.4, 0]
     pos["9"] = [4.4, -0.1]
@@ -13,23 +17,37 @@ def set_own_pos():
     return pos
 
 
-def new_edges(tuple_):
+def new_edges(edge_list):
+    """
+
+    :param edge_list: list of edges which need to be highlighted
+    :return: list of nodes
+    """
     tmp_node = []
-    for elements in tuple_:
+    for elements in edge_list:
         for node_ in elements:
             if node_ not in tmp_node:
                 tmp_node.append(node_)
-    G.remove_edges_from(tuple_)
+    G.remove_edges_from(edge_list)
 
     nx.draw(G, pos=set_own_pos(), with_labels=True, font_weight='bold', node_color="black", edge_color="black",
             font_color="w")
-    G.add_edges_from(tuple_)
+    G.add_edges_from(edge_list)
 
-    nx.draw_networkx_edges(G, pos=set_own_pos(), edgelist=tuple_, edge_color='r')
+    nx.draw_networkx_edges(G, pos=set_own_pos(), edgelist=edge_list, edge_color='r')
     return tmp_node
 
 
 def part3_4():
+    """
+    Finding components of the Graph
+    for each component finding amount of nodes and edges, radius, diameter
+    for each node finding degree of that and eccentricity
+
+    then highlighting random way(path) with length equals to diameter of each component
+    and showing it up
+
+    """
     n = 1
     tmp_edges_global = []
     with open("output.txt", 'w') as out:
@@ -38,30 +56,30 @@ def part3_4():
             tmp = nx.subgraph(G, x)
 
             print(f"-------Begin of the component {n}-------")
-            out.writelines(f"-------Begin of the component {n}-------")
+            out.write(f"-------Begin of the component {n}-------")
             x = sorted(x, key=float)
 
             print(f'nodes = {len(x)} ')
-            out.writelines(f'nodes = {len(x)} ')
+            out.write(f'nodes = {len(x)} ')
             print(f'edges = {len(G.edges(x))}')
-            out.writelines(f'edges = {len(G.edges(x))}')
+            out.write(f'edges = {len(G.edges(x))}')
             eccentr = nx.eccentricity(tmp)
             for node_ in x:
                 print(f"~~~~~~~~Node {node_}~~~~~~~~")
-                out.writelines("~~~~~~~~Node {node_}~~~~~~~~")
+                out.write("~~~~~~~~Node {node_}~~~~~~~~")
                 print(f'''degree: {G.degree[node_]}''')
-                out.writelines(f'''degree: {G.degree[node_]}''')
+                out.write(f'''degree: {G.degree[node_]}''')
                 print(f'''eccentricity: {eccentr[node_]}''')
-                out.writelines(f'''eccentricity: {eccentr[node_]}''')
+                out.write(f'''eccentricity: {eccentr[node_]}''')
             print("")
-            out.writelines("")
+            out.write("")
             diam = nx.diameter(tmp, eccentr)
             print(f'''Radius of the component {n} = {nx.radius(tmp, eccentr)}''')
-            out.writelines(f'''Radius of the component {n} = {nx.radius(tmp, eccentr)}''')
+            out.write(f'''Radius of the component {n} = {nx.radius(tmp, eccentr)}''')
             print(f'''Diameter of the component {n} = {diam}''')
-            out.writelines(f'''Diameter of the component {n} = {diam}''')
-            tmp_edges = []
+            out.write(f'''Diameter of the component {n} = {diam}''')
 
+            tmp_edges = []
             for node1 in x:
                 for node2 in x:
                     for path in (nx.all_simple_edge_paths(tmp, node1, node2)):
@@ -80,7 +98,10 @@ def part3_4():
 
 """
 PART 1
+distribution of given adjacency list of edges written in a data file
+with built-in networkx methods
 """
+
 my_graph = nx.Graph()
 f = open("data.txt", "r")
 plt.figure(figsize=(8, 8))
@@ -95,6 +116,7 @@ plt.close()
 
 """
 PART 2
+Creating an own nodes distribution 
 """
 
 nx.draw(G, pos=set_own_pos(), with_labels=True, font_weight='bold', node_color="black", edge_color="black",
@@ -104,6 +126,15 @@ plt.show()
 plt.close()
 """
 PART 3-4
+
+Finding components of the Graph
+for each component finding amount of nodes and edges, radius, diameter 
+for each node finding degree of that and eccentricity
+    
+then highlighting random way(path) with length equals to diameter of each component
+and showing it up 
+
+
 """
 
 part3_4()
@@ -112,7 +143,10 @@ plt.show()
 plt.close()
 """
 PART 5
+finding a DFS tree by using built-in methods of the "networkx" library  and highlighting edges with a function 
+"new_edges" then showing received picture 
 """
+
 tmp_edges = []
 for el in (nx.dfs_edges(G)):
     tmp_edges.append(el)
